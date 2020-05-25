f838ed1c327a1f34bb4bd1c19ea53e48e40cdf0c
Tristan Rice <rice@fn.lc>
2020-05-25 12:03:16 -0700

    ig: compute all classes ig

--- ig.py
+++ ig.py
@@ -1,10 +1,14 @@
 from captum.attr import IntegratedGradients
 import sys
 
+import numpy as np
 from model import Net, val_transform, trainset, transform
 import torch
 from PIL import Image
 import torchvision.transforms.functional as tf
+
+torch.manual_seed(123)
+np.random.seed(123)
 
 path = sys.argv[-1]
 
@@ -14,6 +18,7 @@ net.load_state_dict(state_dict)
 net.eval()
 
 def save_img(im, path):
+    print(f'saving {path}...')
     max, min = torch.max(im), torch.min(im)
     im = (im-min) / (max-min)
     out_im = tf.to_pil_image((im.squeeze(0)*255).byte())
@@ -26,13 +31,15 @@ baseline = torch.zeros(im.size())
 
 out = net(im)
 klass = torch.argmax(out).item()
-
 print('predicted', trainset.classes[klass], out)
 
-ig = IntegratedGradients(net)
-attributions, delta = ig.attribute(im, baseline, target=klass, return_convergence_delta=True)
+for i, name in enumerate(trainset.classes):
+    ig = IntegratedGradients(net)
+    attributions, delta = ig.attribute(im, baseline, target=i, return_convergence_delta=True)
 
-print('IG Attributions:', attributions)
-print('Convergence Delta:', delta)
+    #print('IG Attributions:', attributions)
+    print('Convergence Delta:', delta)
 
-save_img(attributions, 'ig_attributions.png')
+    save_img(attributions, f'ig_{name}.png')
+    if i == klass:
+        save_img(attributions, 'ig_attributions.png')
